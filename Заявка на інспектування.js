15718c85e29f7bb305a2e3731e060d139c55f3d5
Ihor Melnyk <igor.melnick85@gmail.com>
2023-08-22 09:56:32 +0300

    finish code

--- Заявка на інспектування.js
+++ Заявка на інспектування.js
@@ -111,3 +111,41 @@ function controlDisabled(CODE, disabled = true) {
   control.disabled = disabled;
   EdocsApi.setControlProperties(control);
 }
+
+//4. // Вирахування суми ПДВ заявки
+function onChangelocomotiveInspectionRequest() {
+  let VATpercentage = 0;
+  const attrVATAmount = EdocsApi.getAttributeValue("RequestVATAmmount");
+  const attrVATpercentage = EdocsApi.getAttributeValue("RequestVATPerecent");
+  const attrContractAmount = EdocsApi.getAttributeValue("edocsDocSum");
+  const attrAmountOutVAT = EdocsApi.getAttributeValue("RequestAmmountOutVat");
+
+  switch (attrVATpercentage.value) {
+    case "20%": // if (x === 'если сумма НДС=20%')
+      VATpercentage = 1.2;
+      break;
+
+    case "7%": // if (x === 'если сумма НДС=7%')
+      VATpercentage = 1.07;
+      break;
+  }
+
+  if (attrVATpercentage.value === null || attrContractAmount.value === null) {
+    // если нет ставки НДС и суммы, то укажем ноль в сумме НДС и без НДС
+    attrVATAmount.value = 0;
+    attrAmountOutVAT.value = 0;
+  } else if (VATpercentage == 0) {
+    attrVATAmount.value = 0;
+    attrAmountOutVAT.value = attrContractAmount.value;
+  } else {
+    attrAmountOutVAT.value = Math.floor((100 * attrContractAmount.value) / VATpercentage) / 100;
+    attrVATAmount.value = attrContractAmount.value - attrAmountOutVAT.value;
+  }
+
+  EdocsApi.setAttributeValue(attrVATAmount);
+  EdocsApi.setAttributeValue(attrAmountOutVAT);
+}
+
+function onChangeVATpercentage() {
+  onChangeAuctionScrapMetalSalesContractAmount();
+}
